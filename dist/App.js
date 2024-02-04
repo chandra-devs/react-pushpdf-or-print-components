6ade8ee2c9df65194e1d1f831262f44b1ff55c55
Chandra Kishore <kishorechandra.developer@gmail.com>
2024-02-04 23:55:33 +0530

    Add App component and run script

---
+++ dist/App.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var react_1 = tslib_1.__importDefault(require("react"));
+var react_bootstrap_1 = require("react-bootstrap");
+var index_1 = tslib_1.__importDefault(require("../src/index")); // Import the missing PushComponent module
+var App = function () {
+    return (react_1.default.createElement("div", null,
+        react_1.default.createElement("h1", null, "Hello, Kishore!"),
+        react_1.default.createElement("p", null, "Welcome to your React application with TypeScript."),
+        react_1.default.createElement(index_1.default, { trigger: react_1.default.createElement(react_bootstrap_1.Button, null, "Print"), pushTrigger: react_1.default.createElement(react_bootstrap_1.Button, null, "Save as PDF"), pushPdfTo: {
+                url: 'http://localhost:3000/pdf',
+                method: 'POST',
+            } },
+            react_1.default.createElement("div", null, "Content to print or save as PDF."))));
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
