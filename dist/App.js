47f3bd96da5e9c05423e90deb2826e08d4defc2e
Chandra Kishore <kishorechandra.developer@gmail.com>
2024-02-05 22:58:54 +0530

    Fix import statement for PushComponent module

--- dist/App.js
+++ dist/App.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
 var react_1 = tslib_1.__importDefault(require("react"));
 var react_bootstrap_1 = require("react-bootstrap");
-var index_1 = tslib_1.__importDefault(require("../src/index")); // Import the missing PushComponent module
-// import PushComponent from '../dist/index'; // Import the missing PushComponent module
+// import PushComponent from '../src/index'; // Import the missing PushComponent module
+var index_1 = tslib_1.__importDefault(require("../dist/index")); // Import the missing PushComponent module
 var App = function () {
     return (react_1.default.createElement("div", null,
         react_1.default.createElement("h1", null, "Print / GenPDF"),
