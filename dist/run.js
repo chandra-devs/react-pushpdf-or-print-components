ce8448c0c8cf26d1402dec73ee312d3f7e9ff67f
Chandra Kishore <kishorechandra.developer@gmail.com>
2024-02-05 22:36:11 +0530

    Add new files and update existing files

---
+++ dist/run.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var react_1 = tslib_1.__importDefault(require("react"));
+var react_dom_1 = tslib_1.__importDefault(require("react-dom"));
+var App_1 = tslib_1.__importDefault(require("./App"));
+react_dom_1.default.render(react_1.default.createElement(App_1.default, null), document.getElementById('root'));
+//# sourceMappingURL=run.js.map
